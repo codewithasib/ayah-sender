97578c914b8d1161ec76d3393287eeac53c21d3a
Asib Hossen <dev.asib@proton.me>
2024-08-01 17:56:28 +0600

    changed the version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="ayah_sender",
-    version="0.1",
+    version="0.1.2",
     packages=find_packages(),
     include_package_data=True,
     package_data={
