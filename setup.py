db46388075d29256f7e7b47d7a1b68125fc1d9bc
Asib Hossen <dev.asib@proton.me>
2024-08-01 18:34:13 +0600

    changed the version number back to 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="ayah-sender",
-    version="0.1.3",
+    version="0.1.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={
